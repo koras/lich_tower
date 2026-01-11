d7cb8c06611c8928a66615b8493035a5b29b8506
Глазырев Константин Сергеевич <k.glazyrev@mygiftcard.ru>
2026-01-11 14:27:33 +0300

    peace

---
+++ Assets/Scripts/Heroes/BodyParts/Skeleton/GibPieceClickForwarder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Heroes.BodyParts.Skeleton
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    
+    public class GibsContainer : MonoBehaviour
+    {
+         
+    }
+}
