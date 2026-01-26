cd5e609760e5cbf50be41889f91a05dfbc4ee9f2
Глазырев Константин Сергеевич <k.glazyrev@mygiftcard.ru>
2026-01-27 01:33:33 +0300

    new orien

--- Assets/Scripts/Level/ForceLandscape.cs
+++ Assets/Scripts/Level/ForceLandscape.cs
@@ -1,7 +1,16 @@
-﻿namespace Level
+﻿using UnityEngine;
+
+namespace Level
 {
-    public class ForceLandscape
+    public class ForceLandscape : MonoBehaviour
     {
-        
+        void Awake()
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft; // или LandscapeRight
+            Screen.autorotateToPortrait = false;
+            Screen.autorotateToPortraitUpsideDown = false;
+            Screen.autorotateToLandscapeLeft = true;
+            Screen.autorotateToLandscapeRight = false; // если хочешь только Left
+        }
     }
 }
