f63d974b72648640e6a521b49bb8964ffd7a29ec
Глазырев Константин Сергеевич <k.glazyrev@mygiftcard.ru>
2026-02-01 22:39:00 +0300

    score

---
+++ Assets/Scripts/Ui/HighScoreTable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Spine.Unity;
+using Level.Loading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+
+namespace Ui
+{
+    public class HighscoreTable
+    {
+        [Header("Animations")] [SerializeField]
+        private AnimationReferenceAsset _idle;
+
+    }
+}
